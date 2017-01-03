67c45f56dc18a588b9d1397454ace24c19763db2
Mark <markh4b@hotmail.com>
2017-01-03 13:57:39 +0100

    Test scenario: Fix coordinates

--- Colonies.ocf/Test2.ocs/Script.c
+++ Colonies.ocf/Test2.ocs/Script.c
@@ -9,55 +9,55 @@ func Initialize()
 func DefaultScenario()
 {
 	// 3 tiles, topmost land row
-	Map()->AddHex(4, 4)->SetResource(Resource_Grain)->AddNumberChip(4);
-	Map()->AddHex(6, 6)->SetResource(Resource_Wood)->AddNumberChip(6);
-	Map()->AddHex(8, 8)->SetResource(Resource_Grain)->AddNumberChip(9);
+	Map()->AddHex( 8,  8)->SetResource(Resource_Grain)->AddNumberChip(4);
+	Map()->AddHex(10, 10)->SetResource(Resource_Wood)->AddNumberChip(6);
+	Map()->AddHex(12, 12)->SetResource(Resource_Grain)->AddNumberChip(9);
 
 	// 4 tiles
-	Map()->AddHex( 4, 2)->SetResource(Resource_Brick)->AddNumberChip(2);
-	Map()->AddHex( 6, 4)->SetResource(Resource_Wood)->AddNumberChip(5);
-	Map()->AddHex( 8, 6)->SetResource(Resource_Wool)->AddNumberChip(12);
-	Map()->AddHex(10, 8)->SetResource(Resource_Wool)->AddNumberChip(4);
+	Map()->AddHex( 8,  6)->SetResource(Resource_Brick)->AddNumberChip(2);
+	Map()->AddHex(10,  8)->SetResource(Resource_Wood)->AddNumberChip(5);
+	Map()->AddHex(12, 10)->SetResource(Resource_Wool)->AddNumberChip(12);
+	Map()->AddHex(14, 12)->SetResource(Resource_Wool)->AddNumberChip(4);
 
 	// 5 tiles, middle row
-	Map()->AddHex( 4, 0)->SetResource(Resource_Wool)->AddNumberChip(9);
-	Map()->AddHex( 6, 2)->SetResource(Resource_Brick)->AddNumberChip(8);
-	Map()->AddHex( 8, 4)->AddNumberChip(nil); // Desert
-	Map()->AddHex(10, 6)->SetResource(Resource_Ore)->AddNumberChip(8);
-	Map()->AddHex(12, 8)->SetResource(Resource_Wool)->AddNumberChip(10);
+	Map()->AddHex( 8,  4)->SetResource(Resource_Wool)->AddNumberChip(9);
+	Map()->AddHex(10,  6)->SetResource(Resource_Brick)->AddNumberChip(8);
+	Map()->AddHex(12,  8)->AddNumberChip(nil); // Desert
+	Map()->AddHex(14, 10)->SetResource(Resource_Ore)->AddNumberChip(8);
+	Map()->AddHex(16, 12)->SetResource(Resource_Wool)->AddNumberChip(10);
 
 	// 4 tiles
-	Map()->AddHex( 6, 0)->SetResource(Resource_Wood)->AddNumberChip(3);
-	Map()->AddHex( 8, 2)->SetResource(Resource_Ore)->AddNumberChip(5);
-	Map()->AddHex(10, 4)->SetResource(Resource_Brick)->AddNumberChip(10);
-	Map()->AddHex(12, 6)->SetResource(Resource_Wood)->AddNumberChip(11);
+	Map()->AddHex(10,  4)->SetResource(Resource_Wood)->AddNumberChip(3);
+	Map()->AddHex(12,  6)->SetResource(Resource_Ore)->AddNumberChip(5);
+	Map()->AddHex(14,  8)->SetResource(Resource_Brick)->AddNumberChip(10);
+	Map()->AddHex(16, 10)->SetResource(Resource_Wood)->AddNumberChip(11);
 
 	// 3 tiles, lowest row
-	Map()->AddHex( 8, 0)->SetResource(Resource_Grain)->AddNumberChip(3);
-	Map()->AddHex(10, 2)->SetResource(Resource_Grain)->AddNumberChip(6);
-	Map()->AddHex(12, 4)->SetResource(Resource_Ore)->AddNumberChip(11);
+	Map()->AddHex(12, 4)->SetResource(Resource_Grain)->AddNumberChip(3);
+	Map()->AddHex(14, 6)->SetResource(Resource_Grain)->AddNumberChip(6);
+	Map()->AddHex(16, 8)->SetResource(Resource_Ore)->AddNumberChip(11);
 
 	// ports
-	Map()->AddHex( 2,  4)->AllowTrade(true)->SetResource(Resource_Ore); // Rock, 2:1 
-	Map()->AddHex( 6,  8)->AllowTrade(true)->SetResource(Resource_Wool); // Sheep, 2:1 
-	Map()->AddHex(10, 10)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
-	Map()->AddHex(14, 10)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
-	Map()->AddHex(14,  6)->AllowTrade(true)->SetResource(Resource_Grain); // Wheat, 2:1 
-	Map()->AddHex(12,  2)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
-	Map()->AddHex( 8, -2)->AllowTrade(true)->SetResource(Resource_Brick); // Loam, 2:1 
-	Map()->AddHex( 4, -2)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
-	Map()->AddHex( 2,  0)->AllowTrade(true)->SetResource(Resource_Wood); // Wood, 2:1 
+	Map()->AddHex( 6,  8)->AllowTrade(true)->SetResource(Resource_Ore); // Rock, 2:1 
+	Map()->AddHex(10, 12)->AllowTrade(true)->SetResource(Resource_Wool); // Sheep, 2:1 
+	Map()->AddHex(14, 14)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
+	Map()->AddHex(18, 14)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
+	Map()->AddHex(18, 10)->AllowTrade(true)->SetResource(Resource_Grain); // Wheat, 2:1 
+	Map()->AddHex(16,  6)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
+	Map()->AddHex(12,  2)->AllowTrade(true)->SetResource(Resource_Brick); // Loam, 2:1 
+	Map()->AddHex( 8,  2)->AllowTrade(true)->SetResource(nil); // Any, 3:1 
+	Map()->AddHex( 6,  4)->AllowTrade(true)->SetResource(Resource_Wood); // Wood, 2:1 
 
 	// other sea tiles
-	Map()->AddHex( 4,  6);
 	Map()->AddHex( 8, 10);
-	Map()->AddHex(12, 10);
-	Map()->AddHex(14,  8);
+	Map()->AddHex(12, 14);
+	Map()->AddHex(16, 14);
+	Map()->AddHex(18, 12);
+	Map()->AddHex(18,  8);
 	Map()->AddHex(14,  4);
-	Map()->AddHex(10,  0);
-	Map()->AddHex( 6, -2);
-	Map()->AddHex( 2, -2);
-	Map()->AddHex( 2,  2);
+	Map()->AddHex(10,  2);
+	Map()->AddHex( 6,  2);
+	Map()->AddHex( 6,  6);
 	
 	// Create edges and nodes
 	for (var hex in Map()->GetHexes())
