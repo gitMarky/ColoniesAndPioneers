9cf902821913a602302889f4bb1da7e6bbba7a3a
Mark <markh4b@hotmail.com>
2017-01-05 12:42:53 +0100

    Refactoring: Fix node transformation

--- Colonies.ocf/LibraryHexMap.ocd/Graphics.ocd/HexTile.ocd/Script.c
+++ Colonies.ocf/LibraryHexMap.ocd/Graphics.ocd/HexTile.ocd/Script.c
@@ -6,7 +6,7 @@
  */
 func GetTileWidth()
 {
-	return GetNodeFactor(1, 1000).X;
+	return GetTileFactor(1000).X;
 }
 
 /**
@@ -16,19 +16,48 @@ func GetTileWidth()
  */
 func GetTileHeight()
 {
-	return (GetNodeFactor(0, 1000).Y + GetNodeFactor(1, 1000).Y)/2;
+	return GetTileFactor(1000).Y;
 }
 
 
-func GetNodeFactor(int index, int precision)
+func GetTileFactor(int precision)
 {
-	var nodes = [[0, -50], [42, -24], [252, 24], [0, 50], [-42, 24], [-42, -24]];
-	var hex_node = GetNodeCoordinates(index);
-	var node = nodes[index];
-
-	var node_offset = {X = 50, Y = 0};
-	var hex_offset = {X = HexMap_HexRadius() - 1, Y = 0};
+	var nodes = [[0, -50], [42, -24], [42, 24], [0, 50], [-42, 24], [-42, -24]];
 	
+	var factor_x = 0;
+	var factor_y = 0;
+	
+	for (var index = 0; index < 6; ++index)
+	{
+		var hex_node = GetNodeCoordinates(index);
+		var node = nodes[index];
+	
+		var sign = Sign(node[0]);
+		
+		if (sign == 0) sign = 1;
+		
+		var node_offset = {X = 50 * sign, Y = 0};
+		var hex_offset = {X = (HexMap_HexRadius() - 1) * sign, Y = 0};
+	
+		var factor = GetNodeFactor(node, node_offset, hex_node, hex_offset, precision);
+
+		if (!factor_y || factor.Y > factor_y)
+		{
+			factor_y = factor.Y;
+		}
+
+		if (!factor_x || factor.X > factor_x)
+		{
+			factor_x = factor.X;
+		}
+	}
+	
+	return {X = factor_x, Y = factor_y};
+}
+
+
+func GetNodeFactor(array node, proplist node_offset, hex_node, proplist hex_offset, int precision)
+{
 	return {X = (hex_node.X + hex_offset.X) * precision / (node[0] + node_offset.X),
 	        Y = (hex_node.Y + hex_offset.Y) * precision / (node[1] + node_offset.Y)};
 }
